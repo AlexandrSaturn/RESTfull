afd6657b24a8873354e284b31ebff10d0c386818
AlexandrSaturn <AlexandrSaturn@users.noreply.github.com>
2019-02-15 08:36:39 +0300

    Repository

--- src/main/java/com/RESTfull/repository/UserRepository.java
+++ src/main/java/com/RESTfull/repository/UserRepository.java
@@ -3,7 +3,13 @@ package com.RESTfull.repository;
 import com.RESTfull.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User,Long> {
+import java.util.List;
 
+
+public interface UserRepository extends CrudRepository<User,Long> {
+    List<User> findByEmail(String email);
+    List<User> findByLogin(String login);
+    void deleteById (Long id);
+    User findById(Integer id);
 
 }
