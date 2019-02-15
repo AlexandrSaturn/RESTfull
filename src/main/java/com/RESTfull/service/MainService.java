cf28ec0d963641a5c4eb901d0bdc04b3da872783
AlexandrSaturn <AlexandrSaturn@users.noreply.github.com>
2019-02-15 08:36:54 +0300

    Service

--- src/main/java/com/RESTfull/service/MainService.java
+++ src/main/java/com/RESTfull/service/MainService.java
@@ -1,8 +1,12 @@
 package com.RESTfull.service;
 
+import com.RESTfull.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface MainService {
-
+    Iterable<User> getAll();
+    User getById(Integer id);
+    User save(User user);
+    void deleteById(long id);
 }
--- src/main/java/com/RESTfull/service/MainServiceImpl.java
+++ src/main/java/com/RESTfull/service/MainServiceImpl.java
@@ -1,4 +1,32 @@
 package com.RESTfull.service;
 
+import com.RESTfull.entity.User;
+import com.RESTfull.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
 public class MainServiceImpl implements MainService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Iterable<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User getById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(long id) {
+        userRepository.deleteById(id);
+    }
 }
