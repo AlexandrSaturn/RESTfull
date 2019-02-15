717b5f91387f8bdc0b0175d565f9fd5fdee2ce0c
AlexandrSaturn <AlexandrSaturn@users.noreply.github.com>
2019-02-15 08:36:19 +0300

    Controller

--- src/main/java/com/RESTfull/controller/MainController.java
+++ src/main/java/com/RESTfull/controller/MainController.java
@@ -1,15 +1,28 @@
 package com.RESTfull.controller;
 
-import com.RESTfull.service.MainServiceImpl;
+import com.RESTfull.entity.User;
+import com.RESTfull.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/main")
 public class MainController {
 
     @Autowired
-    private MainServiceImpl mainServiceImpl;
+    private MainService mainService;
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    @ResponseBody
+    public Iterable<User> getAllUsers(){
+        return mainService.getAll();
+    }
+
+    @RequestMapping(value = "/main/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public User getUser(@PathVariable("id") Integer userId) {
+        return mainService.getById(userId);
+    }
 
 }
